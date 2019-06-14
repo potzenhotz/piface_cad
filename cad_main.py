25f3f38e21739e588a06fd652ae36f4c12ca7c3b
Potzenhotz <lukasmuessle@gmail.com>
2019-06-14 17:44:30 +0100

    start working on weather class

--- cad_main.py
+++ cad_main.py
@@ -4,9 +4,7 @@ if sys.version[0] != '3':
 	print ('You must run piface-weather with python3')
 	sys.exit(0)
 import pifacecad
-import api_key
-import json
-from urllib.request import urlopen
+import open_weather as weather
 
 #-----------------------------------------------------------------------
 #Parameters
@@ -25,27 +23,9 @@ def init_cat():
 	cad.lcd.blink_off()
 	return cad
 
-def get_weather_url(open_weater_app_key, lat, lon):
-	raw_url = "https://api.openweathermap.org/data/2.5/weather?"
-	and_url = "&"
-	lat_url = "lat={0}".format(lat)
-	lon_url = "lon={0}".format(lon)
-	lang_url = "lang=de"
-	units_url = "units=metric"
-	id_url = "appid={0}".format(open_weater_app_key)
-	weather_url = raw_url + lat_url + and_url + lon_url + and_url + lang_url + and_url + units_url + and_url + id_url
-	return weather_url
-
-def get_weather(weather_url):
-	jsonFile = urlopen(weather_url)
-	jsonFileContent = jsonFile.read().decode('utf-8')
-	weather_obj = json.loads(jsonFileContent)
-	return weather_obj
-
 def press_button_0(event):
 	temp = weather_obj['main']['temp']
 	return event.chip.lcd.write("Temp: {0}".format(temp))
-
 
 def handlePin(event):
 	event.chip.lcd.clear()
@@ -64,6 +44,4 @@ def start_listener():
 
 if __name__ == "__main__":
 	cad = init_cat()
-	weather_url = get_weather_url(api_key.open_weater_app_key, lat, lon)
-	weather_obj = get_weather(weather_url)
 	start_listener()
---
+++ open_weather.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import json
+from urllib.request import urlopen
+import api_key
+
+#-----------------------------------------------------------------------
+#Parameters
+#-----------------------------------------------------------------------
+num_of_pins=8
+lat=53.86
+lon=10.68
+
+class weather:
+
+	def __init__(self, lat, lon):
+		self.weather_url = self.set_weather_url(api_key.open_weater_app_key, lat, lon)
+		self.weather_obj = self.get_weather(self.weather_url)
+
+	def set_weather_url(self, open_weater_app_key, lat, lon):
+		raw_url = "https://api.openweathermap.org/data/2.5/weather?"
+		and_url = "&"
+		lat_url = "lat={0}".format(lat)
+		lon_url = "lon={0}".format(lon)
+		lang_url = "lang=de"
+		units_url = "units=metric"
+		id_url = "appid={0}".format(open_weater_app_key)
+		weather_url = raw_url + lat_url + and_url + lon_url + and_url + lang_url + and_url + units_url + and_url + id_url
+		return weather_url
+	
+	def get_weather(self, weather_url):
+		jsonFile = urlopen(weather_url)
+		jsonFileContent = jsonFile.read().decode('utf-8')
+		weather_obj = json.loads(jsonFileContent)
+		return weather_obj
+
+
+if __name__=="__main__":
+	weather = weather(lat, lon)
