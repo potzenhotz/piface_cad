1db4d462155ed374ed5e38f8374543db54f9d626
Potzenhotz <lukasmuessle@gmail.com>
2019-06-15 12:48:39 +0100

    weather button 0 finished

--- cad_main.py
+++ cad_main.py
@@ -5,7 +5,11 @@ if sys.version[0] != '3':
 	sys.exit(0)
 import pifacecad
 import open_weather as weather_api
+import time
 
+
+#if ascii error is raised:
+#export PYTHONIOENCODING=utf-8
 #-----------------------------------------------------------------------
 #Parameters
 #-----------------------------------------------------------------------
@@ -27,22 +31,44 @@ class cad:
 
 	def create_weather_list(self):
 		self.weather_list = []
-		self.weather_list.extend(["Temp: {}".format(self.weather.get_temp())])
-		self.weather_list.extend(["Humidity: {}".format(self.weather.get_humidity())])
+		deg_symbol = u'\N{DEGREE SIGN}'
+		condition = self.weather.get_condition()
+		temp = self.weather.get_temp()
+		humidity = self.weather.get_humidity()
+		pressure = self.weather.get_pressure()
+		temp_min = self.weather.get_temp_min()
+		temp_max = self.weather.get_temp_max()
+		wind_speed = self.weather.get_wind_speed()
+		wind_dir = self.weather.get_wind_dir()
+		self.weather_list.extend(["\n{0}".format(condition)])
+		self.weather_list.extend(["{0}\nTemp: {1} C".format(condition, temp)])
+		self.weather_list.extend(["Temp: {0} C\nPress: {1} hPa".format(temp, pressure)])
+		self.weather_list.extend(["Press: {0} hPa\nHumidity: {1}%".format(pressure, humidity)])
+		self.weather_list.extend(["Humidity: {0}%\nW-Speed: {1} m/s".format(humidity, wind_speed)])
+		self.weather_list.extend(["W-Speed: {0} m/s\nW-Dir: {1}".format(wind_speed, wind_dir)])
+		self.weather_list.extend(["W-Dir: {0}\nT_Min: {1} C".format(wind_dir, temp_min)])
+		self.weather_list.extend(["T_Min: {0} C\nT_Max: {1} C".format(temp_min, temp_max)])
+		self.weather_list.extend(["T_Max: {0} C\n{1}".format(temp_max, condition)])
 
-	
 	def press_button_0(self, event):
 		if self.loop_index == len(self.weather_list):
 			self.loop_index = 0
+		if self.loop_index ==0:
+			event.chip.lcd.write("Updating Data:")
+			self.weather.update_weather()
 		return event.chip.lcd.write(self.weather_list[self.loop_index])
 	
 	def handlePin(self, event):
 		event.chip.lcd.clear()
 		if(event.pin_num == 0):
+			self.cad.lcd.backlight_on()
 			self.press_button_0(event)
 			self.loop_index += 1
+		elif(event.pin_num == 4):
+			self.turn_off()
 		else:
-			event.chip.lcd.write("Button:")
+			self.cad.lcd.backlight_on()
+			event.chip.lcd.write("Button: \n")
 			event.chip.lcd.write(str(event.pin_num))
 	
 	def start_listener(self):
@@ -50,11 +76,11 @@ class cad:
 		for i in range(num_of_pins):
 			listener.register(i, pifacecad.IODIR_FALLING_EDGE, self.handlePin)
 		listener.activate()
-
+	
+	def turn_off(self):
+		self.cad.lcd.backlight_off()
 
 if __name__ == "__main__":
-	#cad = init_cat()
-	#start_listener()
 	cad_instance = cad()
 	cad_instance.start_listener()
 	cad_instance.create_weather_list()
--- open_weather.py
+++ open_weather.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import json
-from urllib.request import urlopen
 import api_key
+import requests
 
 # -----------------------------------------------------------------------
 #Parameters
@@ -13,8 +13,8 @@ lon=10.68
 
 class weather:
 	def __init__(self, lat, lon):
-		self.weather_url = self.set_weather_url(api_key.open_weather_api_key, lat, lon)
-		self.weather_dict = self.get_weather(self.weather_url)
+		self.__weather_url = self.set_weather_url(api_key.open_weather_api_key, lat, lon)
+		self.__weather_dict = self.get_weather()
 
 	def set_weather_url(self, open_weater_app_key, lat, lon):
 		raw_url = "https://api.openweathermap.org/data/2.5/weather?"
@@ -27,43 +27,45 @@ class weather:
 		weather_url = raw_url + lat_url + and_url + lon_url + and_url + lang_url + and_url + units_url + and_url + id_url
 		return weather_url
 	
-	def get_weather(self, weather_url):
-		jsonFile = urlopen(weather_url)
-		jsonFileContent = jsonFile.read().decode('utf-8')
-		weather_dict = json.loads(jsonFileContent)
+	def get_weather(self):
+		#resp = requests.get(self.__weather_url, headers={'Cache-Control': 'no-cache'})
+		resp = requests.get(self.__weather_url)
+		weather_dict = resp.json()
 		return weather_dict
+	
+	def update_weather(self):
+		self.__weather_dict = self.get_weather()
 	
 	def get_condition(self):
 		"""weather contains dicts inside a list"""
-		return self.weather_dict["weather"][0]["main"]
+		return self.__weather_dict["weather"][0]["main"]
 
 	def get_temp(self):
-		return self.weather_dict["main"]["temp"]
+		return self.__weather_dict["main"]["temp"]
 
 	def get_temp_min(self):
-		return self.weather_dict["main"]["temp_min"]
+		return self.__weather_dict["main"]["temp_min"]
 
 	def get_temp_max(self):
-		return self.weather_dict["main"]["temp_max"]
+		return self.__weather_dict["main"]["temp_max"]
 
 	def get_humidity(self):
-		return self.weather_dict["main"]["humidity"]
+		return self.__weather_dict["main"]["humidity"]
 
 	def get_pressure(self):
-		return self.weather_dict["main"]["pressure"]
+		return self.__weather_dict["main"]["pressure"]
 
 	def get_wind_speed(self):
-		return self.weather_dict["wind"]["speed"]
+		return self.__weather_dict["wind"]["speed"]
 
 	def get_wind_dir(self):
 		try:
-			return self.weather_dict["wind"]["deg"]
+			return self.__weather_dict["wind"]["deg"]
 		except:
 			return "NaN"
 
 if __name__=="__main__":
 	weather = weather(lat, lon)
-	#print(weather.weather_dict)
 	print(weather.get_condition())
 	print(weather.get_temp())
 	print(weather.get_temp_min())
