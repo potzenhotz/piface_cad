31250b59c6d66a526434ae5a5fafb16adcfd9f62
potzenhotz <lukasmuessle@gmail.com>
2019-06-20 17:08:27 +0200

    create classes for news_api

--- news_api.py
+++ news_api.py
@@ -3,16 +3,40 @@
 import requests
 import api_key
 
+class news_api:
+	def __init__(self, source):
+		self.url = self.set_url(api_key.news_api_key, source)
+		self.news = self.get_news()
 
-raw_url = "https://newsapi.org/v2/top-headlines?sources="
-and_url = "&"
-spiegel_url = "spiegel-online"
-api_url = "apiKey={0}".format(api_key.news_api_key)
-url = raw_url + spiegel_url + and_url + api_url
-resp = requests.get(url)
-headlines = resp.json()
-print(headlines.keys())
-print(headlines["status"])
-print(headlines["totalResults"])
-for i in headlines["articles"]:
-	print(i["title"])
+	def set_url(self, news_api_key, source):
+		raw_url = "https://newsapi.org/v2/top-headlines?sources="
+		and_url = "&"
+		source_url = source
+		api_url = "apiKey={0}".format(news_api_key)
+		return raw_url + source_url + and_url + api_url
+
+	def get_news(self):
+		resp = requests.get(self.url)
+		return resp.json()
+
+	def replace_non_breaking_space(self, string):
+		return string.replace(u'\xa0', u' ')
+
+class spiegel_news(news_api):
+	def __init__(self):
+		super().__init__("spiegel-online")
+		self.headlines = self.get_headlines()
+	
+	def get_headlines(self):
+		headlines = []
+		for article in self.news["articles"]:
+			headline = self.replace_non_breaking_space(article["title"])
+			headlines.append(headline)
+		return headlines
+
+
+if __name__=="__main__":
+	spiegel = spiegel_news()
+	#print(spiegel.get_news())
+	print(spiegel.get_headlines())
+	
